2663d83b16824b3b7056d43d02433ddc166253ff
Felipe Rodrigues Michetti <frmichetti@gmail.com>
2023-12-28 17:20:00 -0300

    added audios

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,39 @@
+function changeAudio(bool){
+  var audio = document.getElementById("audio");
+  if (bool){
+    audio.src="./assets/ymca.mp3"
+  }else{
+    audio.src="./assets/mosca.mp3"
+  }
+  
+  audio.volume = 1
+  audio.play();
+}
+
+
 function toggleMode() {
+  
   const html =
-    document.documentElement /*document é minha página, acesso minha página html através do documentElement*/
+    document.documentElement
 
   if (html.classList.contains("light")) {
-    /*html contem na lista de class 'light'*/
+    
     html.classList.remove(
       "light"
-    ) /*no caso dele conter o nome light, vou acessar a lista do html e remover o nome light */
+    )
+    changeAudio(false)
   } else {
     html.classList.add("light")
+    changeAudio(true)
   }
-
-  /*Todo código abaixa faz o que está no conteúdo acima
-html.classList.toggle('light')
-*/
-
-  //Pegar a tag img
+  
   const img =
     document.querySelector(
       "#profile img"
-    ) /*vai procurar non css o profile img */
-  //substituir a imagem
-  if (html.classList.contains("light")) {
-    /*Se no html conter na classe o light irá atribuir a imagem light */
-    //Se tiver light mode, adicionar a imagem light
+    ) 
+  if (html.classList.contains("light")) {    
     img.setAttribute("src", "./assets/avatar-fefe-light.jpg")
-  } else {
-    /*Se não tiver o light então mantei a imagem sem o light */
-    //Se tiver sem light mode, manter a imagem normal
+  } else {    
     img.setAttribute("src", "./assets/avatar-fefe-dark.jpg")
   }
 }
